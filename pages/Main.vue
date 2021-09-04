7f7ecc349817adce28c0d7729efdda06db339532
SongMinJun <dkaskgkdua@gmail.com>
2021-09-04 12:04:05 +0900

    1. 상품목록 표시 기능 구현

--- pages/Main.vue
+++ pages/Main.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
     <p>메인 페이지 입니다</p>
-    <ProductList
-    >
-    </ProductList>
+    <div>
+      <ul>
+        <li
+          v-for="product in products"
+          :key="product.id"
+        >
+          <img :src="product.imageUrl" :alt="product.name"/>
+          <p>{{ product.name }}</p>
+          <p>{{ product.price }}</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -15,7 +24,10 @@ export default {
   async asyncData() {
     const response = await axios.get('http://localhost:3000/products')
     console.log(response);
-    const products = response.data;
+    const products = response.data.map(item => ({
+      ...item,
+        imageUrl: `${item.imageUrl}?random=${Math.random()}`
+    }));
     return { products }
   },
   data() {
